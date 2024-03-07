1afe79bcd7b82ab90677ea516c5f143cf8936b97
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-03-07 20:43:22 +0800

    add footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,6 @@
+import { MoveUpRight } from "lucide-react";
+import Link from "next/link";
+
 export const Footer = () => {
   return (
     <div className="py-5 md:py-10 border-t border-slate-300 border-opacity-30 flex items-start justify-between mt-auto">
@@ -5,9 +8,20 @@ export const Footer = () => {
         Developed & Designed by{" "}
         <span className="font-bold text-slate-300">Fakhrul</span>{" "}
       </p>
-      <div className="text-sm md:text-lg space-y-2">
-        <p>Contribute on GitHub</p>
-        <p>About</p>
+      <div className="text-sm md:text-lg space-y-2 flex flex-col">
+        <Link
+          href={"https://github.com/0xffakhrul/cari-idea"}
+          className="flex items-center gap-2 hover:text-slate-300 transition duration-150"
+        >
+          Contribute to GitHub
+          <MoveUpRight width={16} height={16} />
+        </Link>
+        <Link
+          href={"/about"}
+          className="flex items-center gap-2 hover:text-slate-300 transition duration-150"
+        >
+          About <MoveUpRight width={16} height={16} />
+        </Link>
       </div>
     </div>
   );
