aab2313481db9e3f509f1c106081c4f60039ecae
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-03-07 11:07:26 +0800

    ui

---
+++ components/ui/textarea.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          "flex min-h-[80px] w-full rounded-md focus:outline-none border-b border-slate-300 border-opacity-30 bg-transparent backdrop-blur-lg px-1 py-3 text-base placeholder:text-zinc-400 disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Textarea.displayName = "Textarea";
+
+export { Textarea };
